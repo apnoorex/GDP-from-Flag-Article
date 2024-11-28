5fe9772d6205357275aec5b0f489378f2c88e5fc
Andrey Puzanov <puzanov.ee@gmail.com>
2024-11-28 17:41:33 +0300

    Remove outliers

--- muslim_countries_ranges.py
+++ muslim_countries_ranges.py
@@ -3,6 +3,9 @@ import pandas as pd
 
 countries = pd.read_csv('in_data/world_flags_2024.csv')
 
+countries = countries[countries['Population'] > 100000]
+countries = countries[countries['GDPPerCapita'] > 1000]
+
 print('Dataset min:', countries['GDPPerCapita'].min())
 print('Dataset max:', countries['GDPPerCapita'].max())
 print('Dataset mean:', countries['GDPPerCapita'].mean())
